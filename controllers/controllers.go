ae649a3b30698c584da3ca531bb22af48796b9f3
matheus borges <matheusborgessampaio@outlook.com>
2024-09-02 10:50:37 -0300

    Terminei tudo certo

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -16,7 +16,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-type", "application/json")
 	var p []models.Personalidade
 	database.DB.Find(&p)
 	json.NewEncoder(w).Encode(p)
@@ -45,9 +45,50 @@ func RetornarUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 
 func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
 
-	var NovaP models.Personalidade
-	json.NewDecoder(r.Body).Decode(&NovaP)
-	database.DB.Create(&NovaP)
-	json.NewEncoder(w).Encode(NovaP)
+	var NovaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&NovaPersonalidade)
+	database.DB.Create(&NovaPersonalidade)
+	json.NewEncoder(w).Encode(NovaPersonalidade)
 
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	id, err := strconv.ParseInt(vars["id"], 10, 64) // O 10 é a base numerica utilizada
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		errorMessage := fmt.Sprintf("Erro transformar Id: %v", err)
+		fmt.Println(errorMessage)
+
+		return
+	}
+
+	var p models.Personalidade
+	database.DB.Delete(&p, id)
+	json.NewEncoder(w).Encode(p)
+
+}
+
+func EditarUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	id, err := strconv.ParseInt(vars["id"], 10, 64) // O 10 é a base numerica utilizada
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		errorMessage := fmt.Sprintf("Erro transformar Id: %v", err)
+		fmt.Println(errorMessage)
+
+		return
+	}
+
+	var p models.Personalidade
+	database.DB.First(&p, id)
+	json.NewDecoder(r.Body).Decode(&p)
+	database.DB.Save(&p)
+	json.NewEncoder(w).Encode(p)
 }
